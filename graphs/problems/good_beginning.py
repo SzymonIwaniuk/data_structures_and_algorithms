2362b8c6b7356c4ee3e112ca6d44767cedb192ca
SzymonIwaniuk <s.f.iwaniuk@gmail.com>
2025-03-29 14:46:08 +0100

    ✅ add graphs/problems/good_beginning.py

--- graphs/problems/good_beginning.py
+++ graphs/problems/good_beginning.py
@@ -1,6 +1,38 @@
 """
 Wierzcholek v w grafie skierowanym nazywamy tak zwanym dobrym
-jezeli kazdy i inny wierzcholek mozna osiagnac sciezka skierowana
-wychodzaca z v. Zaproponowac algorytm, ktory dla podanego grafu
-stwierdza czy G posiada dobry poczatek; lista sasiedztwa
+poczatkiem jezeli kazdy i inny wierzcholek mozna osiagnac sciezka
+skierowana wychodzaca z v. Zaproponowac algorytm, ktory dla podanego
+grafu stwierdza czy G posiada dobry poczatek; lista sasiedztwa;
 """
+
+
+def beginning(G):
+    def dfs_visit(i):
+        nonlocal G, times, time, visited
+        visited[i] = True
+
+        for v in G[i]:
+            if not visited[v]:
+                dfs_visit(v)
+
+        time += 1
+        times[i] = time
+
+    n = len(G)
+    times = [-1 for _ in range(n)]
+    time = 0
+    visited = [False for _ in range(n)]
+
+    for i in range(n):
+        if not visited[i]:
+            dfs_visit(i)
+
+    for i in range(n):
+        if times[i] == n:
+            s = i
+            break
+
+    visited = [False for _ in range(n)]
+    time = 0
+    dfs_visit(s)
+    return time == n
