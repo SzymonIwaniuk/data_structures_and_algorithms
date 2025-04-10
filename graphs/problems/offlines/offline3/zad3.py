589347f7cdbcbf59c1ded71088b8f26e7efad646
SzymonIwaniuk <s.f.iwaniuk@gmail.com>
2025-04-10 09:50:06 +0200

    fix to offline 3

--- graphs/problems/offlines/offline3/zad3.py
+++ graphs/problems/offlines/offline3/zad3.py
@@ -1,9 +1,8 @@
 from zad3testy import runtests
 from collections import deque
 
-
-def longer(G: list[list[int]], s: int, t: int) -> any:
-    # step 1: bfs
+def longer( G: list[list[int]], s: int, t: int ) -> any:
+    #step 1: bfs
     # O(V + E)
     n = len(G)
     distance = [0 for _ in range(n)]
@@ -22,31 +21,38 @@ def longer(G: list[list[int]], s: int, t: int) -> any:
                 distance[u] = distance[v] + 1
                 queue.append(u)
 
-    # step 2: check is exist path from s -> t
+    #step 2: check is exist path from s -> t
     if not visited[t]:
         return None
 
-    # step 3: check vertex neightbours in t -> s path
-    # O(V + E)
-
+    #step 3: check vertex neightbours in t -> s path
+    #O(V + E)
     t_copy = t
+    prev = None
+
     while t_copy != None:
-        for u in G[t]:
-            if parent[u] != t_copy and distance[u] >= distance[t_copy]:
-                return (parent[t_copy], t_copy)
+        another_edges = 0
+        flag = False
+        for u in G[t_copy]:
+            if distance[u] >= distance[t_copy] and parent[t_copy] != u and prev != u:
+                flag = True
+            if distance[u] <= distance[t_copy] - 1 and parent[t_copy] != u:
+                another_edges += 1
+        if another_edges == 0 and flag:
+            return(parent[t_copy], t_copy)
+        prev = t_copy
         t_copy = parent[t_copy]
 
     return None
 
-    # Total complexity O(V + E)
+    #Total complexity O(V + E)
 
+# zmien all_tests na True zeby uruchomic wszystkie testy
+runtests( longer, all_tests = True )
 
-if __name__ == "__main__":
-
-    # zmien all_tests na True zeby uruchomic wszystkie testy
-    runtests(longer, all_tests=True)
-
+if __name__ == '__main__':
     # G = [ [1, 2],
     #     [0, 2],
     #     [0, 1] ]
     # print(longer(G,0,2))
+    pass
