3c8c2b182ee40e169d2fcb903b00e53923ad24d5
SzymonIwaniuk <s.f.iwaniuk@gmail.com>
2025-04-13 22:29:08 +0200

    add graphs lab5

---
+++ graphs/labs/lab5/euler_cycle_matrix.py
@@ -0,0 +1,29 @@
+# 1 Prosze zaimplementowac algorytm znjadujacy cykl Eulera (rep macierzowa graf spojny stopnie parzyste)
+def dfs_euler(G: list[list[int]]) -> list[int]:
+    n = len(G)
+    U = [0 for _ in range(n)]
+    cycle = []
+
+    def dfs_visit(G: list[list[int]], v: int) -> any:
+        while U[v] < n:
+            u = U[v]
+            U[v] += 1
+            if G[v][u] == 1:
+                G[v][u] = G[u][v] = 0
+                dfs_visit(G, u)
+        cycle.append(v)
+
+    dfs_visit(G, 0)
+
+    return cycle
+
+
+if __name__ == "__main__":
+    G1 = [
+        [0, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1],
+        [1, 1, 0, 1, 1],
+        [1, 1, 1, 0, 1],
+        [1, 1, 1, 1, 0],
+    ]
+    print(dfs_euler(G1))
