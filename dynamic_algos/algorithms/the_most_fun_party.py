b1b69be8c50adbe566896002fce4b5ab868c41d9
SzymonIwaniuk <s.f.iwaniuk@gmail.com>
2025-05-21 11:15:14 +0200

    feat: add solutions to dp

---
+++ dynamic_algos/algorithms/the_most_fun_party.py
@@ -0,0 +1,39 @@
+from typing import TypeVar
+
+EmpType = TypeVar("EmpType", bound="Emp")
+
+
+class Emp:
+    def __init__(self, fun: int) -> None:
+        self.emp = []
+        self.fun = fun
+        self.f_val = -1
+        self.g_val = -1
+
+    def f(self: EmpType) -> int:
+        if self.f_val != -1 and self.g_val != -1:
+            return max(self.f_val, self.g_val)
+
+        self.f_val = self.fun
+        self.g_val = 0
+
+        for employee in self.emp:
+            employee.f()
+            self.f_val += employee.g_val
+            self.g_val += max(employee.f_val, employee.g_val)
+
+        return max(self.f_val, self.g_val)
+
+
+if __name__ == '__main__':
+    senior = Emp(5)
+    mid1 = Emp(3)
+    mid2 = Emp(6)
+    junior1 = Emp(1)
+    junior2 = Emp(1)
+
+    senior.emp = [mid1, mid2]
+    mid1.emp = [junior1]
+    mid2.emp = [junior2]
+
+    print(senior.f())
