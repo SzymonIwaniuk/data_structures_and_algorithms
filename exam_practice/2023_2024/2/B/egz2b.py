85b5728c8288b03f222c51b7901e3ffbc7b7a8b4
SzymonIwaniuk <s.f.iwaniuk@gmail.com>
2025-06-27 12:33:18 +0200

    feat: Update content

--- exam_practice/2023_2024/2/B/egz2b.py
+++ exam_practice/2023_2024/2/B/egz2b.py
@@ -1,10 +1,61 @@
 from egz2btesty import runtests
+from collections import deque
+
+inf = float('inf')
+
+
+def build_adjacency_list(edges, a):
+    # O(m)
+    graph = [[] for _ in range(a + 1)]
+    # O(m)
+    for x, y, d, typ in edges:
+        graph[x].append((y, d, typ))
+        graph[y].append((x, d, typ))
+
+    return graph
 
 
 def tory_amos(E, A, B):
-    # tu prosze wpisac wlasna implementacje
-    return -1
+    a = max(max(x, y) for x, y, d, typ in E)
+    graph = build_adjacency_list(E, a)
+    queue = deque()
+    times = [[inf] * 2 for _ in range(a + 1)]
+    times[A][0] = 0
+    times[A][1] = 0
+
+    types = {'P': 0, 'I': 1}
+
+    for station, d, t in graph[A]:
+        n = types[t]
+        if times[station][n] > d:
+            times[station][n] = d
+            queue.append((station, d, n))  # stacja, czas, rozstaw kol
+
+    while queue:
+        station, time, t = queue.popleft()
+
+        for new_station, d, new_t in graph[station]:
+            n = types[new_t]
+            #print(n)
+            if n == t:
+                if n == 0:
+                    new_time = time + 10 + d
+                else:
+                    new_time = time + 5 + d
+                    # print(new_time, new_station, station, d, type)
+                if times[new_station][n] > new_time:
+                    times[new_station][n] = new_time
+                    queue.appendleft((new_station, new_time, n))
+
+            else:
+                new_time = time + 20 + d
+                if times[new_station][n] > new_time:
+                    times[new_station][n] = new_time
+                    queue.append((new_station, new_time, n))
+
+    #print(times)
+    return min(times[B])
 
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests(tory_amos, all_tests=False)
+runtests(tory_amos, all_tests=True)
